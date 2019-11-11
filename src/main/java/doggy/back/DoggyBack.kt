8efe747e328200fb4e4c8a35c4824dba6d63abe1
Brandone Martins <brm@octo.com>
2019-11-11 15:09:55 +0100

    Bean pour flyway

--- src/main/java/doggy/back/DoggyBack.kt
+++ src/main/java/doggy/back/DoggyBack.kt
@@ -1,6 +1,8 @@
 package doggy.back
 
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import org.flywaydb.core.Flyway
+import org.flywaydb.core.api.configuration.FluentConfiguration
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.Bean
@@ -10,6 +12,13 @@ class DoggyBack {
 
     @Bean
     fun objectMapper() = jacksonObjectMapper()
+
+    @Bean
+    fun flyway(): Flyway {
+        return Flyway.configure()
+            .baselineOnMigrate(true)
+            .load()
+    }
 }
 
 fun main(args: Array<String>) {
