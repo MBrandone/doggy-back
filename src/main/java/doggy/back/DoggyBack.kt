f59bfe5e0efd6064dda795d01dc5ce9287cb5bef
Benoit Meriaux <benoit.meriaux@outlook.com>
2019-12-22 17:22:24 +0100

    chore: add schedule task to keep front app awake on heroku and by side effect we stay alive too

--- src/main/java/doggy/back/DoggyBack.kt
+++ src/main/java/doggy/back/DoggyBack.kt
@@ -3,14 +3,27 @@ package doggy.back
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.annotation.Bean
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.web.client.RestTemplate
+import org.springframework.web.client.getForObject
+
 
 @SpringBootApplication
 class DoggyBack {
-
     @Bean
     fun objectMapper() = jacksonObjectMapper()
 
+    @Bean
+    fun restTemplate():RestTemplate {
+        return RestTemplateBuilder().build()
+    }
+
+    @Scheduled(fixedDelay=600000)
+    fun pingFront(restTemplate: RestTemplate) {
+        restTemplate.getForObject<String>("https://doggy-skool-app.herokuapp.com/")
+    }
 }
 
 fun main(args: Array<String>) {
