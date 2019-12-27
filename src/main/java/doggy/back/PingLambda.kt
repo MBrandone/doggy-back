551a61491db73737a22bb39e2e07a3c45cd0a728
Benoit Meriaux <benoit.meriaux@outlook.com>
2019-12-27 11:48:23 +0100

    chore: improve ping

--- src/main/java/doggy/back/PingLambda.kt
+++ src/main/java/doggy/back/PingLambda.kt
@@ -2,6 +2,7 @@ package doggy.back
 
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.web.client.RestTemplate
@@ -14,10 +15,27 @@ class PingLambda(
 
     private val LOGGER: Logger = LoggerFactory.getLogger(PingLambda::class.java)
 
+    @Value("\${ping.doggyFrontHost}")
+    lateinit var doggyFrontHost: String
+
+    @Value("\${ping.doggyBackHost}")
+    lateinit var doggyBackHost: String
+
     @Scheduled(fixedDelay = 600000)
-    fun pingFront() {
-        restTemplate.getForObject<String>("https://doggy-skool-app.herokuapp.com")
-        restTemplate.getForObject<String>("https://doggy-skool-app.herokuapp.com/doggies")
-        LOGGER.info("Ping back & front")
+    fun ping() {
+        kotlin.runCatching {
+            restTemplate.getForObject<String>(doggyFrontHost)
+        }.onSuccess {
+            LOGGER.info("Ping front on $doggyFrontHost")
+        }.onFailure { e ->
+            LOGGER.error("Fail to ping front on $doggyFrontHost", e)
+        }
+        kotlin.runCatching {
+            restTemplate.getForObject<String>("$doggyBackHost/docs/api-docs.json")
+        }.onSuccess {
+            LOGGER.info("Ping back on $doggyBackHost")
+        }.onFailure { e ->
+            LOGGER.error("Fail to ping back on $doggyFrontHost", e)
+        }
     }
 }
